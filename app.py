b7e74c118a27e5296ebca11387cea47e5651bc93
Pablobaptista11111 <96923809+Pablobaptista11111@users.noreply.github.com>
2025-11-21 11:49:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -199,7 +199,7 @@ async def worker_logic(forcar_atualizacao=False, origem="Desconhecido"):
         return
 
     total = len(lista_urls)
-    num_robos = 50
+    num_robos = 5
     adicionar_log(f"🚀 DISPARANDO {num_robos} ROBÔS EM {total} URLS...")
     
     semaphore = asyncio.Semaphore(num_robos) 
